922a6493fea060def4b0d9c6514339834366e3b7
Kez0X <luka.baudrant@gmail.com>
2024-05-07 09:19:46 +0200

    fix :app.py

--- PORTEFOLIO/app.py
+++ PORTEFOLIO/app.py
@@ -5,18 +5,14 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/projects')
-def projects():
-    return render_template("projects.html")
-
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'), 404
 
 if __name__ == "__main__":
-    app.run(port=3003)
+    app.run(port=3009)
 
 # les feuilles de style, scripts, images et autres éléments qui ne seront jamais générés dynamiquement doivent être dans le dossier static,
 # les fichiers HTML doivent être dans le dossier templates,
 # les tests doivent être dans le dossier tests,
-# le fichier views.py contient les différentes routes de l'application
+# le fichier app.py contient les différentes routes de l'application
